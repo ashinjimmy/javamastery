e3db512774df8b6bec8e173518ad68af1ddaf447
Ashin Jimmy <ashinjimmy@yahoo.com>
2024-12-01 23:49:54 +0000

    new commit

---
+++ src/com/java/interview/stringOp/AllPossibleSubstrings.java
@@ -0,0 +1,25 @@
+package com.java.interview.stringOp;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AllPossibleSubstrings {
+
+	public static void main(String[] args) {
+		String input = "AmentaAshin";
+		allPossibleSubstrings(input);
+	}
+
+	private static void allPossibleSubstrings(String input) {
+		List<String> list = new ArrayList<>();
+		for (int i = 0; i < input.length(); i++) {
+			for (int j = i + 1; j <= input.length(); j++) {
+				list.add(input.substring(i, j));
+			}
+		}
+
+		System.out.println(list);
+		System.out.println(list.size());
+	}
+
+}
