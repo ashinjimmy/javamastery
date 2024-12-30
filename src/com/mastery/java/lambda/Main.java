6e2a322e34b6f253be6112f10b1a15c33b55cef7
Ashin Jimmy <ashinjimmy@yahoo.com>
2024-12-30 21:36:09 +0000

    New commit

--- src/com/mastery/java/lambda/Main.java
+++ src/com/mastery/java/lambda/Main.java
@@ -1,5 +1,8 @@
 package com.mastery.java.lambda;
 
+import java.util.function.Consumer;
+import java.util.function.Function;
+
 /**
  * Main class to demonstrate the use of a lambda expression with the Calculator
  * interface.
@@ -13,5 +16,20 @@ public class Main {
 		// Performing the addition and displaying the result
 		int result = calculator.add(10, 45);
 		System.out.println("The sum is: " + result);
+		
+		
+		Function<Integer, Boolean> functionInterface = (n) -> {return (n%2==0)?true:false;};
+		System.out.println("The result is -- "+functionInterface.apply(64));
+		
+		
+		Function<String, String> upperCaseFunction2 = (n) -> {return n.toUpperCase();};
+		System.out.println("The converted word -- "+upperCaseFunction2.apply("Ashin"));
+		
+		Function<String, String> upperCaseFunction1 = (n) -> {return n.toUpperCase();};
+		System.out.println("The converted word -- "+upperCaseFunction1.apply("Amanta"));
+		
+		Consumer<String> messageConsumer = (a)-> {System.out.println(a);};
+		messageConsumer.accept("Message from the consumer !!!!");
+		
 	}
 }
